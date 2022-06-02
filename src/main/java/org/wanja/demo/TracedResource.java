d292563a1d30aa6efb53bd22acd4e2a9e202a62a
Tom Berrey <tberrey@redhat.com>
2022-06-01 20:26:08 -0500

    Added TraceIdResponseInjector.java

--- src/main/java/org/wanja/demo/TracedResource.java
+++ src/main/java/org/wanja/demo/TracedResource.java
@@ -6,7 +6,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import org.jboss.logging.Logger;
 
-@Path("/person")
+@Path("/trace")
 public class TracedResource {
 
     private static final Logger LOG = Logger.getLogger(PersonResource.class);
